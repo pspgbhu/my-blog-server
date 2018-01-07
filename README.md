dfbbf635b85db70ea772ba1d04bfbe0e7410a336
hucong <hucong@jd.com>
2018-01-07 23:22:29 +0800

    feat: init project

---
+++ README.md
@@ -0,0 +1,5 @@
+## 环境安装
+
+pm2 install pm2-logrotate  # 安装
+pm2 set pm2-logrotate:retain 30    # 保留服务器 30 天日志
+pm2 set pm2-logrotate:workerInterval 60   #  60秒检测一次，根据 max\_size 的值来检测是否需要日志分割。
---
+++ app.js
@@ -0,0 +1,39 @@
+const Koa = require('koa');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logger');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = new Koa();
+
+// error handler
+onerror(app);
+
+// middlewares
+app.use(bodyparser({
+  enableTypes: ['json', 'form', 'text'],
+}));
+app.use(json());
+app.use(logger());
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// routes
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx);
+});
+
+module.exports = app;
