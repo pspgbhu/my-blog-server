ab809b701d0de4c63818bc172b50ff6912973aef
hucong <hucong@jd.com>
2018-01-14 20:15:45 +0800

    feat: midify mysql

--- libs/create.sql
+++ libs/create.sql
@@ -1,11 +1,13 @@
 USE blog;
 
 CREATE TABLE `blog`.`messages` (
-	`id` int(3) NOT NULL AUTO_INCREMENT,
-  `article` varchar(100) NOT NULL,
-	`name` varchar(100) NOT NULL,
-	`email` varchar(100) DEFAULT NULL,
+	`id` int(8) NOT NULL AUTO_INCREMENT,
+  `article` varchar(128) NOT NULL,
+	`name` varchar(128) NOT NULL,
+	`email` varchar(128) NOT NULL,
 	`text` text DEFAULT NULL,
+  `replayId` int(8) DEFAULT NULL,
 	`create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` bit NOT NULL DEFAULT b'1',
 	PRIMARY KEY (`id`)
 ) ENGINE=`InnoDB`;
