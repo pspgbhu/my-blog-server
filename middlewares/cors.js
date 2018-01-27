37c248c4625f4c65275d5d581fb6ae2e58e2b7c8
hucong <hucong@jd.com>
2018-01-27 23:42:40 +0800

    feat: cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,6 @@
 module.exports = () => async (ctx, next) => {
   ctx.set({
-    'Access-Control-Allow-Origin': ctx.request.header.origin,
+    'Access-Control-Allow-Origin': 'http://blog.pspgbhu.me',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
     'Access-Control-Allow-Credentials': true,
     'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
